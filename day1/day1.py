d7034bb8c0c54e11c0e7600e1146e0b341f55d93
bclavie <ben@clavie.eu>
2020-12-01 13:53:47 +0100

    day1 one liner

--- day1/day1.py
+++ day1/day1.py
@@ -5,9 +5,10 @@ entries = open('./input.txt', 'r').readlines()
 entries = [int(x) for x in entries]
 
 def solve_day1(entries, size):
-    for comb in combinations(entries, size):
-        if sum(comb) == 2020:
-            return math.prod(comb)
+    return math.prod([x for x in combinations(entries, size) if sum(x) == 2020][0])
+    # for comb in combinations(entries, size):
+    #     if sum(comb) == 2020:
+    #         return math.prod(comb)
 
 print('Part 1: {0}'.format(solve_day1(entries, 2)))
 print('Part 2: {0}'.format(solve_day1(entries, 3)))
