3bf1152ef24ef4ead67be57a701e704be20a4193
bclavie <ben@clavie.eu>
2020-12-02 11:08:11 +0100

    day 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,14 @@
 from itertools import combinations
 import math
 
-entries = open('./input.txt', 'r').readlines()
-entries = [int(x) for x in entries]
-
 def solve_day1(entries, size):
     return math.prod([x for x in combinations(entries, size) if sum(x) == 2020][0])
     # for comb in combinations(entries, size):
     #     if sum(comb) == 2020:
     #         return math.prod(comb)
 
-print('Part 1: {0}'.format(solve_day1(entries, 2)))
-print('Part 2: {0}'.format(solve_day1(entries, 3)))
+if __name__ == '__main__':
+    entries = open('./input.txt', 'r').readlines()
+    entries = [int(x) for x in entries]
+    print('Part 1: {0}'.format(solve_day1(entries, 2)))
+    print('Part 2: {0}'.format(solve_day1(entries, 3)))
---
+++ day2/day2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+from operator import xor
+
+def solve_day2(entries):
+    valid_count_p1, valid_count_p2 = 0, 0
+    for entry in entries:
+        words = entry.strip().split()
+        min_char, max_char = [int(x) for x in words[0].split('-')]
+        policy_char = words[1][0]
+        password = words[2]
+        counts = Counter(password)
+        if min_char <= counts[policy_char] <= max_char:
+            valid_count_p1 += 1
+        if xor((password[min_char-1] == policy_char), (password[max_char-1] == policy_char)):
+            valid_count_p2 += 1
+    
+    return valid_count_p1, valid_count_p2
+
+
+if __name__ == '__main__':
+    entries = open('./input.txt', 'r').readlines()
+    print('Part 1: {0}\nPart 2: {1}'.format(*solve_day2(entries)))
+    # print('Part 2: {0}'.format(solve_day1(entries, 3)))
